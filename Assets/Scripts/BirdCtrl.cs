77e417fe9934afe3cd70b62c251dfb2ec64be13f
Kirill Dudnik <prog.me.2017@gmail.com>
2020-10-13 17:51:24 +0600

    The project was created! Imported bird sprites, pipes, background, ground, and sounds. Created a script for the movement of birds and pipes (no pipes spawn). Configured the build for Android. -------------- Проект создан! Импорировал спрайты птицы, труб, фона, земли, и звуки. Создал скрипт движения птицы и трубы (спавна труб нет). Настроил сборку под Андроид.

---
+++ Assets/Scripts/BirdCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody2D))]
+public class BirdCtrl : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField] private float _flyUpForce = 4.0f;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        FlyUp();
+    }
+
+    private void FlyUp()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.velocity = Vector2.up * _flyUpForce;
+        }
+    }
+}
---
+++ Assets/Scripts/PipeMoveCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveCtrl : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2.0f;
+    void Update()
+    {
+        transform.Translate(-1.0f * _speed * Time.deltaTime, 0, 0);
+    }
+}
